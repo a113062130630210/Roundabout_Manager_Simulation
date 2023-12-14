701f99bea685412712b514cac6d8f62c80c8b9e1
a113062130630210 <tienhsinglin.a@gmail.com>
2023-12-14 14:12:31 +0800

    add possion arrival

--- solve/constants.hpp
+++ solve/constants.hpp
@@ -14,4 +14,7 @@
 // should be a positive number
 #define TIME_GAP 1.5
 
+// should be a positive number
+#define MEAN_INTERARRIVAL_TIME 10
+
 #define EXIT(s) do { std::cerr << s << std::endl; exit(1); } while (0)
--- solve/main.cpp
+++ solve/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <random>
 
 #include "roundabout.hpp"
 #include "roundabout_manager.hpp"
@@ -8,6 +9,21 @@
 
 int main() {
   roundabout r(8, { 10, 10, 10, 10, 10, 10, 10, 10 });
+
+  // generate possion arrival test case
+  std::vector<double> arrival_times;
+  std::default_random_engine generator;
+  std::exponential_distribution<double> distribution(MEAN_INTERARRIVAL_TIME);
+  for (int number_of_vehicles = 1 ; number_of_vehicles <= 5 ; number_of_vehicles++) {
+      double arrival_time_diff = distribution(generator);
+      double arrival_time = 0;
+      if (arrival_times.size() == 0) {
+          arrival_times.push_back(arrival_time);
+      }
+      else {
+          arrival_times.push_back(arrival_time[arrival_times.size() - 1] + arrival_time_diff);
+      }
+  }
 
   // TODO: test wrap around
   std::vector<vehicle> vs;
